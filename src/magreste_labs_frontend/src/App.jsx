6004a328c8b14e123d5cd25ceeaec60d2fd3b5d9
fidelina arteaga <fidearte@gmail.com>
2025-01-10 23:52:22 -0300

    Initial commit.

---
+++ src/magreste_labs_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { magreste_labs_backend } from 'declarations/magreste_labs_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    magreste_labs_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
